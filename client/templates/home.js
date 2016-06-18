6581721ae440175f1185c1a8984890c0dbbc2095
Jay Nanavati <jaysnanaavti@hotmail.co.uk>
2016-06-18 22:02:15 +0100

    test

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,5 +1,14 @@
 Template.home.helpers({
     fullName: function() {
         return Meteor.user().profile.name;
+    },
+    repoName: function() {
+        return "2048 Demo"
+    },
+    buildPassing: function() {
+        return true;
+    },
+    userImageURL: function() {
+        return "https://avatars2.githubusercontent.com/u/2691401?s=72";
     }
 });

c6216c50f56d9439e867861fdc84fb7ed849be8f
Jay Nanavati <jaysnanaavti@hotmail.co.uk>
2016-06-18 20:05:23 +0100

    test

--- client/templates/home.js
+++ client/templates/home.js
@@ -1,8 +1,5 @@
 Template.home.helpers({
-    firstName: function() {
-        return Meteor.user().profile.first_name;
-    },
     fullName: function() {
-        return Meteor.user().profile.first_name + " " + Meteor.user().profile.last_name;
+        return Meteor.user().profile.name;
     }
 });

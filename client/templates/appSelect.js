fd08b7bdd46ce4a3d99314d23a6b8824184b1a72
Jay Nanavati <jaysnanaavti@hotmail.co.uk>
2016-06-19 01:23:34 +0100

    test

--- client/templates/appSelect.js
+++ client/templates/appSelect.js
@@ -1,8 +1,25 @@
-Template.appSelect.helpers({
-    username: function() {
-        return Meteor.user().services.github.username
-    },
-    userImageURL: function() {
-        return "https://avatars2.githubusercontent.com/u/2691401?s=72";
-    },
+if (Meteor.isClient) {
+    Template.appSelect.helpers({
+        username: function() {
+            return Meteor.user().services.github.username
+        },
+        userImageURL: function() {
+            return "https://avatars2.githubusercontent.com/u/2691401?s=72";
+        }
+    });
+}
+
+if (Meteor.isServer) {
+    Meteor.methods({
+        getUserRepos: function() {
+            this.unblock();
+            return "repos from server"
+        }
+    });
+}
+
+Template.appSelect.onCreated(function() {
+    Meteor.call("getUserRepos", function(error, results) {
+        console.log(results); //results.data should be a JSON object
+    });
 });
--- client/templates/home.js
+++ client/templates/home.js
@@ -1,21 +1,23 @@
-Template.home.helpers({
-    fullName: function() {
-        return Meteor.user().profile.name;
-    },
-    repoName: function() {
-        return "2048 Demo"
-    },
-    buildPassing: function() {
-        return true;
-    },
-    userImageURL: function() {
-        return Meteor.user().services.github.avatar_url;
-    },
-    appName: function() {
-        return null;
-    }
-});
+if (Meteor.isClient) {
+    Template.home.helpers({
+        fullName: function() {
+            return Meteor.user().profile.name;
+        },
+        repoName: function() {
+            return "2048 Demo"
+        },
+        buildPassing: function() {
+            return true;
+        },
+        userImageURL: function() {
+            return Meteor.user().services.github.avatar_url;
+        },
+        appName: function() {
+            return null;
+        }
+    });
 
-Template.home.onRendered(function() {
+    Template.home.onRendered(function() {
 
-});
+    });
+}
